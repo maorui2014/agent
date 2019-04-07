78b59ad52691f93bcd47e51d60d90b590597b849
MaoRui2014 <maorui2014@foxmail.com>
2019-04-07 20:43:41 +0800

    b

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react"
+import { BrowserRouter as Router } from "react-router-dom"
 import { config, AppContext } from "./AppContext"
-import Frame from "./frame"
+import Frame from "../frame"
 
 export default function App() {
 	const state = useState(config)
 
 	return (
 		<AppContext.Provider value={ state }>
-			<Frame />
+			<Router>
+				<Frame />
+			</Router>
 		</AppContext.Provider>
 	)
 }
--- src/components/frame/index.js
+++ src/components/frame/index.js
@@ -1,17 +1,56 @@
-import React from "react"
-import TopBar from "./topbar"
-import SideBar from "./sidebar"
-import Content from "./content"
+import React, { useEffect, useState } from "react"
+import { Route, Switch, Link } from "react-router-dom"
 
+import RoomSource from "../room-source"
+import Client from "../client"
 import "./index.scss"
 
+const { remote, ipcRenderer } = window.require("electron")
+const win = remote.getCurrentWindow()
+
 export default function Frame() {
+	const [isMaximize, setIsMaximize] = useState(false)
+
+	useEffect(() => {
+		const onMaximize = (event, isMaximize) => setIsMaximize(isMaximize)
+		ipcRenderer.on("maximize", onMaximize)
+		return () => ipcRenderer.removeListener("maximize", onMaximize)
+	}, [])
+
+	function doMaximize() {
+		isMaximize ? win.unmaximize() : win.maximize()
+		setIsMaximize(!isMaximize)
+	}
+
 	return (
 		<div className="main-container">
-			<TopBar />
+			<div className="topbar-container">
+				<div className="titlebar-drag-region" />
+				<div className="window-appicon" onDoubleClick={ () => win.close() } />
+				<div className="window-title">中原地产 [服务端]</div>
+				<div className="control-button use-information" />
+				<div className="control-button settings" />
+				<div className="control-button window-minimize" onClick={ () => win.minimize() } />
+				<div className={ `control-button window-${isMaximize ? "restore" : "maximize"}` } onClick={ doMaximize } />
+				<div className="control-button window-close" onClick={ () => win.close() } />
+			</div>
 			<div className="content-container-wrapper">
-				<div className="sider-container"><SideBar /></div>
-				<div className="content-container"><Content /></div>
+				<div className="sider-container">
+					<Link className="main-menu room-source-management" to="/room-source">
+						<div className="mgt-icon" />
+						<span>房源管理</span>
+					</Link>
+					<Link className="main-menu client-management" to="/client">
+						<div className="mgt-icon" />
+						<span>客户管理</span>
+					</Link>
+				</div>
+				<div className="content-container">
+					<Switch>
+						<Route path="/client" component={ Client } />
+						<Route path={ ["/", "/room-source"] } component={ RoomSource } />
+					</Switch>
+				</div>
 			</div>
 		</div>
 	)
